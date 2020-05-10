7e2a48acda5bb3791c73452888a6b1983f113e64
Maciej Ryszka <maciejryszka@gmail.com>
2020-05-10 15:56:36 +0200

    add new styles; add new scripts

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 const name = "Maciek";
 
 console.log(`Witaj ${name}!`);
+
+const articleParagraph = document.querySelector(".article__text--js");
+
+if (articleParagraph) {
+  articleParagraph.innerHTML =
+    "Ta część artykułu notatki została zmodyfikowana z użyciem JavaScript.";
+}
