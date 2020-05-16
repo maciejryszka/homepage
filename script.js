bfff4446cd14f6df04e22f38fa776bade6aebeba
Maciej Ryszka <maciejryszka@gmail.com>
2020-05-17 01:55:17 +0200

    add new article; add greet function; update image; add styles normalize

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
-const name = "Maciek";
-
-console.log(`Witaj ${name}!`);
-
 const articleParagraph = document.querySelector(".article__text--js");
 
 if (articleParagraph) {
   articleParagraph.innerHTML =
     "Ta część artykułu została zmodyfikowana z użyciem JavaScript.";
 }
+
+const name = "Maciej";
+const age = 27;
+
+function greet(userName, userAge) {
+  console.log(`Witaj ${userName}! Twój wiek to: ${userAge}.`);
+}
+
+const user = {
+  name: "Karol",
+  age: 37,
+  greet() {
+    console.log(`Cześć ${this.name}! Twój wiek to: ${this.age}.`);
+  },
+};
+
+greet(name, age);
+user.greet();
