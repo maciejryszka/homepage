0ec0162859879459f7173d88d5ec587588707f71
Maciej Ryszka <maciejryszka@gmail.com>
2020-05-26 23:09:36 +0200

    adding hamburger menu

--- script.js
+++ script.js
@@ -22,3 +22,14 @@ const user = {
 
 greet(name, age);
 user.greet();
+
+const hamburgerMenuButton = document.querySelector(
+  ".header-navigation__menu-button"
+);
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+
+function showHamburgerMenu() {
+  hamburgerMenu.classList.toggle("hamburger-menu--active");
+}
+
+hamburgerMenuButton.addEventListener("click", showHamburgerMenu);
